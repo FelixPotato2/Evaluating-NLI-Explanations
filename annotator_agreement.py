9d113c7c31db097797f0c45bb0ab461c892af39f
ester-vdw <esterfenna@gmail.com>
2026-01-26 11:44:28 +0100

    worked in annotator_agreement to get lengths

--- annotator_agreement.py
+++ annotator_agreement.py
@@ -98,26 +98,91 @@ from evaluate_LLMs import Get_manual_evaluation_problems
 
 # Read from file and parse JSON
 with open("gold.json", "r") as f:
-    data = json.load(f)
+    gold_answers = json.load(f)
+
+with open("manual.json", "r") as f2:
+    script_answers =json.load(f2)
+
+def get_len_lists(file):
+    left_len_list =[]
+    right_len_list =[]
+    for dict in gold_answers:
+        for answers in dict.values(): #this is only one...
+            #print("answers")
+            #print(answers)
+            for answer_group in answers:
+                #print("group")
+                #print(answer_group)
+                for answer in answer_group["left"]:
+                    #print(answer.split())
+                    left_len_list.append(len(answer.split()))
+                for answer in answer_group["right"]:
+                    right_len_list.append(len(answer.split()))
+                #left_len_list.append(len(answer_group["left"]))
+            # right_len_list.append(len(answer_group["right"]))
+    return left_len_list, right_len_list
+
+def get_len_lists_script():
+    sript_pr, script_answers = Get_manual_evaluation_problems()
+    left_len_list =[]
+    right_len_list =[]
+    for answers in script_answers.values(): #this is only one...
+            #print("answers")
+            #print(answers)
+        for answer_group in answers:
+                #print("group")
+                #print(answer_group)
+            for answer in answer_group["left"]:
+                    #print(answer.split())
+                left_len_list.append(len(answer))
+            for answer in answer_group["right"]:
+                right_len_list.append(len(answer))
+                #left_len_list.append(len(answer_group["left"]))
+            # right_len_list.append(len(answer_group["right"]))
+    return left_len_list, right_len_list
+
+    
+
+def print_len_info(left_len_list, right_len_list):
+    avg_len_left = sum(left_len_list) / len(left_len_list)
+    avg_len_right = sum(right_len_list)/len(right_len_list)
+    print(f"max left: {max(left_len_list)}")
+    print(f"max right: {max(right_len_list)}")
+    print(f"average len left: {avg_len_left}")
+    print(f"average len right: {avg_len_right}")
+
+left_len_list_g, right_len_lists_g = get_len_lists(gold_answers)
+left_len_list_s, right_len_lists_s = get_len_lists_script()
+print("gold set----------------")
+print_len_info(left_len_list_g, right_len_lists_g)
+print("script set -----------------")
+print_len_info(left_len_list_s, right_len_lists_s)
+
+
+
 
 #print(data)
 #print(type(data))
-print(len(data))
-sript_pr, script_answers = Get_manual_evaluation_problems()
 
-for ID in script_answers:
-    print(ID)
-    print(script_answers[ID])
-for problem in data:
-    for ID in problem.keys(): #its a bit dumb in how we structured it but this should be just one
-        script_ans = script_answers[ID]
-        for answer_dict in problem[ID]:
-            left_set = set()
-            right_set =set()
-            for word in answer_dict["left"]:
-                if word not in ["a", "an", "the"]:
-                    left_set.add(word.lower())
-            for word in answer_dict["right"]:
-                if word not in ["a", "an", "the"]:
-                    right_set.add(word.lower())
+
+#print(len(data))
+
+
+
+
+# for ID in script_answers:
+#     print(ID)
+#     print(script_answers[ID])
+# for problem in data:
+#     for ID in problem.keys(): #its a bit dumb in how we structured it but this should be just one
+#         script_ans = script_answers[ID]
+#         for answer_dict in problem[ID]:
+#             left_set = set()
+#             right_set =set()
+#             for word in answer_dict["left"]:
+#                 if word not in ["a", "an", "the"]:
+#                     left_set.add(word.lower())
+#             for word in answer_dict["right"]:
+#                 if word not in ["a", "an", "the"]:
+#                     right_set.add(word.lower())
             
